49338ebda3679d9c6bf5e330399b5d6f844c1243
Zahid67635 <54507558+Zahid67635@users.noreply.github.com>
2020-09-20 20:43:58 +0600

    Update settings.py

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ class settings:
 
         self.screen_width = 1200
         self.screen_height = 600
-        self.bg_color = (0,0,0)
+        self.bg_color = (230,230,230)
         self.ship_speed = 1.5
 
         #bullet settings
@@ -12,4 +12,5 @@ class settings:
         self.bullet_speed = 1.0
         self.bullet_width = 3
         self.bullet_height= 15
-        self.bullet_color =(255,255,255)
+        self.bullet_color =(0,0,0)
+        self.bullets_allowed = 5
