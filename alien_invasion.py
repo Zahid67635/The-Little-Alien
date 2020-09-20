ce4f8e44bf75bb619d4ec262637bd20f2830b885
Zahid67635 <54507558+Zahid67635@users.noreply.github.com>
2020-09-20 20:42:28 +0600

    Update alien_invasion.py
    aliens added...

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 from settings import settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 class AlienInvasion:
     def __init__(self):
         pygame.init()
@@ -10,16 +11,18 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode(
         (self.settings.screen_width,self.settings.screen_height))
 
-        pygame.display.set_caption("kursi- The Little Monster")
+        pygame.display.set_caption("kursi - The Little Monster")
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+        self._create_fleet()
     def run_game(self):
         while 1:
             self._check_events()
             self.ship.update()
-            self._update_screen()
             pygame.display.flip()
-            self.bullets.update()
+            self._update_bullets()
+            self._update_screen()
     def _check_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -43,15 +46,36 @@ class AlienInvasion:
          elif event.key == pygame.K_LEFT:
              self.ship.moving_left = False
     def _fire_bullet(self):
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
+        self.aliens.draw(self.screen)
+    def _update_bullets(self):
+        self.bullets.update()
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+    def _create_fleet(self):
+        alien = Alien(self)
+        alien_width = alien.rect.width
+        available_space_x = self.settings.screen_width - (2 * alien_width)
+        number_aliens_x = available_space_x//(2*alien_width)
 
+        for alien_number in range(number_aliens_x):
+            self._create_alien(alien_number)
 
+    def _create_alien(self,alien_number):
+        alien = Alien(self)
+        alien_width = alien.rect.width
+        alien.x = alien_width + 2*alien_width*alien_number
+        alien.rect.x = alien.x
+        self.aliens.add(alien)
 if __name__=='__main__':
     ai = AlienInvasion()
     ai.run_game()
