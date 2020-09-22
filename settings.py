3c3827fbc51d9eac539802c650ce3b45027fc86d
Zahid67635 <54507558+Zahid67635@users.noreply.github.com>
2020-09-22 19:11:11 +0600

    Update settings.py

--- settings.py
+++ settings.py
@@ -6,11 +6,16 @@ class settings:
         self.screen_height = 600
         self.bg_color = (230,230,230)
         self.ship_speed = 1.5
+        self.ship_limit = 3
 
         #bullet settings
 
-        self.bullet_speed = 1.0
+        self.bullet_speed = 1.5
         self.bullet_width = 3
         self.bullet_height= 15
         self.bullet_color =(0,0,0)
-        self.bullets_allowed = 5
+        self.bullets_allowed = 20
+
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        self.fleet_direction = 1
